ca55d89b933eef0ec4df4a3c870016b678c8b8f7
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2025-07-17 13:51:36 +0700

    feat(HeroSection): add wallet connection check and toast notification for asset tokenization

--- containers/marketing/landing/hero-section/index.tsx
+++ containers/marketing/landing/hero-section/index.tsx
@@ -1,10 +1,25 @@
+
 import React from "react";
 import Hero from "./hero";
 import Spline from "@splinetool/react-spline/next";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useConnectedWallet } from "@/hooks/wallet/useConnectedWallet";
+import { toast } from "@/hooks/use-toast";
+
 
 export default function HeroSection() {
+  const connectedWallet = useConnectedWallet();
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (!connectedWallet.connected) {
+      e.preventDefault();
+      toast({
+        title: "Please log in",
+        description: "You must connect your wallet to tokenize your assets.",
+        variant: "destructive",
+      });
+    }
+  };
   return (
     <div className="relative w-full h-screen flex flex-col items-center justify-center bg-[#000000] overflow-hidden px-4 sm:px-6 lg:px-8">
       <div className="absolute h-[414px] w-full top-[-100px]">
@@ -12,7 +27,7 @@ export default function HeroSection() {
       </div>
       <div className="transform-gpu hero-eclipse h-[2500px] w-screen left-1/2 -top-[1500px] -translate-x-1/2 absolute mix-blend-color-dodge"></div>
       <Hero />
-      <div className="relative z-0 items-center justify-center lg:items-start  mx-auto mt-10 lg:mt-0 lg:absolute lg:top-60 lg:left-50 flex flex-col gap-2 px-4 sm:px-6 lg:px-0">
+      <div className="relative items-center justify-center lg:items-start  mx-auto mt-10 lg:mt-0 lg:absolute lg:top-60 lg:left-50 flex flex-col gap-2 px-4 sm:px-6 lg:px-0">
         <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold z-50 pointer-events-none text-center lg:text-left">
           <span className="text-primary">Crion: Launch like</span>
           <br />
@@ -22,13 +37,13 @@ export default function HeroSection() {
           Smart scores. Clean UI.
         </p>
         <Button variant="crion" className="w-fit" asChild>
-          <Link href="/token/create">Tokenize Your Assets</Link>
+          <Link href="/token/create" onClick={handleClick}>Tokenize Your Assets</Link>
         </Button>
       </div>
       {/* <Spline
-        className="absolute z-10 w-full h-full top-16 lg:inset-10 lg:top-14 lg:left-100 opacity-50 lg:opacity-100 hidden lg:block"
-        scene="https://prod.spline.design/GEgZWYXC0EjvS3Vs/scene.splinecode"
-      /> */}
+          className="absolute z-10 w-full h-full top-16 lg:inset-10 lg:top-14 lg:left-100 opacity-50 lg:opacity-100 hidden lg:block"
+          scene="https://prod.spline.design/GEgZWYXC0EjvS3Vs/scene.splinecode"
+        /> */}
     </div>
   );
 }
